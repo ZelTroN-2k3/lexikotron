3e8955a6d034ee30372bfb660f582f1bb665022a
ZelTroN-2k3 <zeltron_workgroups@hotmail.com>
2019-02-01 18:24:17 +0100

    Old_adminglossary corection Langage ??
    this correction removes the multiple line per language concern in the glossary edition.
    Bug test prestashop 1.16.1.15 Bug White page..

---
+++ controllers/admin/Old_adminglossary.php
@@ -0,0 +1,234 @@
+<?php
+
+include_once _PS_MODULE_DIR_ . 'lexikotron/models/Glossary.php';
+
+class AdminGlossaryController extends ModuleAdminController
+{
+    /**<?php
+include_once _PS_MODULE_DIR_ . 'lexikotron/models/Glossary.php';
+class AdminGlossaryController extends ModuleAdminController
+{
+    /**
+     * @see ModuleAdminController::__construct()
+     */
+    public function __construct()
+    {
+        $this->table      = 'lexikotron';
+        $this->className  = 'Glossary';
+        $this->identifier = "id_lexikotron";
+        $this->lang = true;
+        $this->bootstrap  = true;
+        $this->context = Context::getContext();
+        parent::__construct();
+    }
+    /**
+     * @see ModuleAdminController::initProcess()
+     */
+    public function initProcess()
+    {
+        $this->bulk_actions = array(
+            'delete' => array(
+                'text'    => $this->l('Delete selected'),
+                'confirm' => $this->l('Delete selected items?'),
+            ),
+        );
+        parent::initProcess();
+    }
+    /**
+     * @see AdminController::renderList()
+     */
+    public function renderList()
+    {
+        $this->fields_list = array(
+            'id_lexikotron' => array(
+                'title' => '#',
+            ),
+            'name'          => array(
+                'title' => $this->l('Texte'),
+            ),
+            'active'        => array(
+                'title'  => $this->l('Valider'),
+                'active' => 'status',
+            ),
+        );
+        $this->actions = array('edit', 'delete');
+        $lists = parent::renderList();
+        parent::initToolbar();
+        return $lists;
+    }
+    /**
+     * @see AdminController::renderForm()
+     */
+    public function renderForm()
+    {
+        $this->fields_form = array(
+            'tinymce' => true,
+            'legend'  => array(
+                'title' => $this->l('Glossary'),
+                //'image' => '../img/admin/cog.gif',
+                'icon' => 'icon-plus-sign-alt'
+            ),
+            'input'   => array(
+                array(
+                    'type'  => 'text',
+                    'lang'  => true,
+                    'label' => $this->l('Name:'),
+                    'name'  => 'name',
+                ),
+                array(
+                    'type'         => 'textarea',
+                    'label'        => $this->l('Description:'),
+                    'name'         => 'description',
+                    'lang'         => true,
+                    'autoload_rte' => true,
+                ),
+                array(
+                    'type'     => 'switch',
+                    'label'    => $this->l('Enabled'),
+                    'name'     => 'active',
+                    'required' => true,
+                    'class'    => 't',
+                    'is_bool'  => true,
+                    'values'   => array(
+                        array(
+                            'id'    => 'glossary_on',
+                            'value' => 1,
+                            'label' => $this->l('Yes'),
+                        ),
+                        array(
+                            'id'    => 'glossary_off',
+                            'value' => 0,
+                            'label' => $this->l('No'),
+                        ),
+                    ),
+                ),
+            ),
+            'submit'  => array(
+                'title' => $this->l('Save'), // This is the button that saves the whole fieldset.
+                'class' => 'btn btn-default pull-right'
+            ),
+        );
+        if (!($obj = $this->loadObject(true))) {
+            return;
+        }
+        return parent::renderForm();
+    }
+}
+     * @see ModuleAdminController::__construct()
+     */
+    public function __construct()
+    {
+        $this->table      = 'lexikotron';
+        $this->className  = 'Glossary';
+        $this->identifier = "id_lexikotron";
+        $this->bootstrap  = true;
+
+        $this->_join = 'LEFT JOIN `' . _DB_PREFIX_ . 'lexikotron_lang` ll ON (a.`id_lexikotron` = ll.`id_lexikotron`)';
+
+        $this->_select = 'll.*';
+
+        parent::__construct();
+    }
+
+    /**
+     * @see ModuleAdminController::initProcess()
+     */
+    public function initProcess()
+    {
+        $this->bulk_actions = array(
+            'delete' => array(
+                'text'    => $this->l('Delete selected'),
+                'confirm' => $this->l('Delete selected items?'),
+            ),
+        );
+
+        parent::initProcess();
+    }
+
+    /**
+     * @see AdminController::renderList()
+     */
+    public function renderList()
+    {
+        $this->fields_list = array(
+            'id_lexikotron' => array(
+                'title' => '#',
+            ),
+            'name'          => array(
+                'title' => $this->l('Texte'),
+            ),
+            'active'        => array(
+                'title'  => $this->l('Valider'),
+                'active' => 'status',
+            ),
+        );
+
+        $this->actions = array('edit', 'delete');
+		$this->lang = $this->context->language->id;
+        $lists = parent::renderList();
+
+        parent::initToolbar();
+
+        return $lists;
+    }
+
+    /**
+     * @see AdminController::renderForm()
+     */
+    public function renderForm()
+    {
+        $this->fields_form = array(
+            'tinymce' => true,
+            'legend'  => array(
+                'title' => $this->l('Glossary'),
+                //'image' => '../img/admin/cog.gif',
+                'icon' => 'icon-plus-sign-alt'
+            ),
+            'input'   => array(
+                array(
+                    'type'  => 'text',
+                    'lang'  => true,
+                    'label' => $this->l('Name:'),
+                    'name'  => 'name',
+                ),
+                array(
+                    'type'         => 'textarea',
+                    'label'        => $this->l('Description:'),
+                    'name'         => 'description',
+                    'lang'         => true,
+                    'autoload_rte' => true,
+                ),
+                array(
+                    'type'     => 'switch',
+                    'label'    => $this->l('Enabled'),
+                    'name'     => 'active',
+                    'required' => true,
+                    'class'    => 't',
+                    'is_bool'  => true,
+                    'values'   => array(
+                        array(
+                            'id'    => 'glossary_on',
+                            'value' => 1,
+                            'label' => $this->l('Yes'),
+                        ),
+                        array(
+                            'id'    => 'glossary_off',
+                            'value' => 0,
+                            'label' => $this->l('No'),
+                        ),
+                    ),
+                ),
+            ),
+            'submit'  => array(
+                'title' => $this->l('Save'), // This is the button that saves the whole fieldset.
+                'class' => 'btn btn-default pull-right'
+            ),
+        );
+
+        if (!($obj = $this->loadObject(true))) {
+            return;
+        }
+
+        return parent::renderForm();
+    }
+}
